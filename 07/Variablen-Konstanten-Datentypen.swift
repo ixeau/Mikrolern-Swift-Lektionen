4b00e89c17b6a4faa2ac3f2dcab1c6b91c006ebd
IXEAU <ixeau@users.noreply.github.com>
2018-06-01 21:49:59 +0200

    Basics

--- 07/Variablen-Konstanten-Datentypen.swift
+++ 07/Variablen-Konstanten-Datentypen.swift
@@ -1,25 +1,20 @@
 /*:
- 
  Mikrolern Swift
  # Basics
  **Konstanten, Variablen & Datentypen**
  */
 import Cocoa
 
-
 var nachname = "Lexow"
 nachname = "Mustermann"
-
 
 let geburtstag = "04.06.1987" // String
 let geburtsjahr = 1987 // Integer
 let koerperTemperatur = 36.8 // Double, Float
 let fuehrerschein = true // Bool, Boolean
 
-
 // Mehrere Variablen gleichzeitig deklarieren — geht, aber meist unleserlich!
 let vorname = "Martin", verheiratet = false
-
 
 // Emojis als Variablenname
 let 🦄😺 = "Einhornkatze"
